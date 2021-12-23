b09359a7472416e0aec9370f2f31b8be4d53f1ca
adrian <cabrera23.adrian@gmail.com>
2021-12-23 20:53:43 +0100

    new map

--- Practice/ui.R
+++ Practice/ui.R
@@ -5,23 +5,38 @@ ui <- fluidPage(
   # Application title
   titlePanel("Malnutrition summary"),
   navbarPage("Menu", id = "panels",
+            
              tabPanel("Geographical distribution of malnutrition",
-                      sidebarLayout(
-                        sidebarPanel(
+         
+                      tags$style(type = "text/css", "#mapplot {height: calc(100vh - 180px) !important;}"),
+                      tags$style("
+        #controls {
+          opacity: 0.8;
+        }
+        #controls:hover{
+          opacity: 1;
+        }
+               "),
+                      leafletOutput("mapplot",height  = '100%')%>% withSpinner(),
+                      
+                      absolutePanel(id='controls',height = "auto",width = 300,bottom = "auto",right = 40, fixed = TRUE,top = 150,
+                                    class = "panel panel-default",
+                                  
+                        
                           
                           selectInput("MalnutritionType2", "Malnutrition type",
                                       choices = listTypeMal)
                           
-                        ),
-                        mainPanel(
+                        
+                        
                           
-                          leafletOutput("mapplot")%>% withSpinner(),
-                          textOutput("prueba")
-                          
+
                           
                           
-                        ),
-                        fluid = TRUE) 
+                        
+                        ) 
+                      
+                      
                       
              )
              ,
